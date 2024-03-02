76e39f266cf59798d9cc9078151340b554167a72
jon94 <jonathan.lim@datadoghq.com>
2024-03-02 15:03:52 +0800

    refactored shell

--- datadog-101-docker/run_script.sh
+++ datadog-101-docker/run_script.sh
@@ -6,14 +6,14 @@ duration=30
 # Path to your Python script
 python_script_path="generate_load_script/load.py"
 
-# Calculate the end time
-end_time=$((SECONDS + duration))
+# Run the Python script in the background
+python3 "$python_script_path" &
 
-# Run the Python script in a loop until the specified duration
-while [ $SECONDS -lt $end_time ]; do
-    # Run the Python script
-    python3 "$python_script_path"
+# Get the process ID of the last background command
+pid=$!
 
-    # Sleep for a short interval before running the script again
-    sleep 1
-done
+# Sleep for the specified duration
+sleep $duration
+
+# Terminate the background process
+kill $pid     
