56baa88f35eb0306064eccffe4981ab529786411
jon94 <jonathan.lim@datadoghq.com>
2024-03-02 16:21:51 +0800

    refactored create monitor

---
+++ datadog-101-docker/create_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change directory to the create_monitor directory
+cd create_monitor
+
+# Iterate through all Python files in the directory
+for file in *.py; do
+    echo "Running $file"
+    python "$file"
+    echo "Completed $file"
+done
